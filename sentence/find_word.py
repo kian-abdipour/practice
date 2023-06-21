9ed81c85b22c4936af47bc76066962d4bc661a79
kian <wk111111111kw@gmail.com>
2023-06-21 19:27:29 +0330

    update file = find_word

--- sentence/find_word.py
+++ sentence/find_word.py
@@ -2,7 +2,7 @@ from custom_exseption import NoSentence
 
 
 # This function get sentence and make a list of word that are in sentence.
-def get_sentence():
+def make_list_word():
     print("Please type a sentence.")
     input_sentence = input(": ")
     list_word = input_sentence.split(" ")
@@ -10,10 +10,7 @@ def get_sentence():
 
 
 # This function get the word and check it is in sentence or not.
-def get_and_find_word():
-    print("If it's finish type (q).")
-    list_word = get_sentence()
-
+def get_and_find_word(list_word):
     print("\nOk, type word that you want.")
     input_word = input(": ")
 
@@ -30,24 +27,25 @@ def get_and_find_word():
 
 
 # This function run the program and check the error
-def run_and_check_error():
-    list_word = get_and_find_word()
+def run_and_check_error(no_sentence):
+    print("If it's finish type (q).")
+    list_word = get_and_find_word(make_list_word())
     while list_word != ["q"]:
         try:
 
             if len(list_word) == 1 and list_word != ["q"]:
-                raise NoSentence("")
+                raise no_sentence("")
 
             else:
                 pass
 
-        except NoSentence:
+        except no_sentence:
             print("\nNoSentence_Error: You should type sentence not word.\n")
-        list_word = get_and_find_word()
+        list_word = get_and_find_word(make_list_word())
 
 
 if __name__ == "__main__":
-    run_and_check_error()
+    run_and_check_error(NoSentence)
 
 else:
     print("You should run this function in main file")
