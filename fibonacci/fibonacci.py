7cd09c71931bec541de144ed1c73f656d278e225
kian <wk111111111kw@gmail.com>
2023-08-15 00:21:14 +1100

    Update fibonacci

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -13,26 +13,27 @@ def get_number_fibonacci():
 
 
 # Recursion
-def calculate_fibonacci_Numbers(number_fibonacci, numbers):
-    if number_fibonacci == 1 or number_fibonacci == 2:
-        return
+def calculate_fibonacci_Numbers(number_fibonacci, numbers, counter):
+    if number_fibonacci == 0 or number_fibonacci == 1 or number_fibonacci == 2:
+        return "finish"
 
     else:
         first_number = numbers[-1]
         second_number = numbers[-2]
         new_number = first_number + second_number
         numbers.append(new_number)
-        calculate_fibonacci_Numbers(number_fibonacci - 1, numbers)
-        print(new_number)
-        return new_number
+        counter += 1
+        print(counter, " _ ",  new_number)
+        return calculate_fibonacci_Numbers(number_fibonacci - 1, numbers, counter)
 
 
 def main():
     progress = True
     while progress:
         number_fibonacci = get_number_fibonacci()
-        print(calculate_fibonacci_Numbers(number_fibonacci, [0, 1]))
-        print(0)
+        print(1, " _ ", 0)
+        print(2, " _ ", 1)
+        print(calculate_fibonacci_Numbers(number_fibonacci, [0, 1], 2))
 
         if number_fibonacci == "q":
             progress = False
