c307246e6ebe6d84032990f520505874a1365f2f
kian <wk111111111kw@gmail.com>
2023-11-06 17:31:43 +0330

    Add __init__ #Task done

---
+++ restaurant/__init__.py
@@ -0,0 +1,7 @@
+from super_admin import SuperAdmin
+from item import Item
+from category import Category
+from admin import Admin
+from order import Order
+from customer import Customer
+
--- restaurant/main.py
+++ restaurant/main.py
@@ -1,13 +1,7 @@
 from sqlalchemy import create_engine
 from base import Base
-from super_admin import SuperAdmin
-from item import Item
-from category import Category
-from admin import Admin
-from order import Order
-from customer import Customer
+import __init__
 
-
-engine = create_engine("postgresql+psycopg2://kian:bmw1386z4@localhost:5432/restaurant")
+engine = create_engine("postgresql+psycopg2://kian:bmw1386z4@localhost:5432/restaurant", echo=True)
 Base.metadata.create_all(engine)
 
