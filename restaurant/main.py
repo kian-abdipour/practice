16a1530ef7178f85d07cdbb6ac7e04f0616a4a25
kian <wk111111111kw@gmail.com>
2023-11-20 18:18:05 +0330

    add models directory without __init__ #Task done

--- restaurant/main.py
+++ restaurant/main.py
@@ -1,14 +1,20 @@
 from sqlalchemy import create_engine
 
-from restaurant.models import superadmin, admin, customer, category, item, order, order_item, base
+from restaurant.models import order, order_item, base
+from restaurant.models.admin import Admin
+from restaurant.models.category import Category
+from restaurant.models.customer import Customer
+from restaurant.models.item import Item
+from restaurant.models.superadmin import SuperAdmin
 
 engine = create_engine('postgresql+psycopg2://kian:bmw1386z4@localhost:5432/restaurant', echo=True)
 
-print(superadmin,
-      admin,
-      customer,
-      category,
-      item,
+
+print(SuperAdmin,
+      Admin,
+      Customer,
+      Category,
+      Item,
       order,
       order_item,
       base)
