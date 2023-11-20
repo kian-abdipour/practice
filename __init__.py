44c96fafd691eeaa7000d927103849d075444388
kian <wk111111111kw@gmail.com>
2023-11-20 18:06:21 +0330

    add models directory without __init__.py last push was fail again

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from superadmin import SuperAdmin
-from item import Item
-from category import Category
-from admin import Admin
-from order import Order
-from customer import Customer
-from order_item import OrderItem
-
--- restaurant/main.py
+++ restaurant/main.py
@@ -1,6 +1,15 @@
 from sqlalchemy import create_engine
-from base import Base
-import __init__
+
+from restaurant.models import superadmin, admin, customer, category, item, order, order_item, base
 
 engine = create_engine('postgresql+psycopg2://kian:bmw1386z4@localhost:5432/restaurant', echo=True)
+
+print(superadmin,
+      admin,
+      customer,
+      category,
+      item,
+      order,
+      order_item,
+      base)
 
