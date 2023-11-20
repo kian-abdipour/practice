6f22727bfdca736deb919cd2f66a718cb13e1099
kian <wk111111111kw@gmail.com>
2023-11-20 18:23:26 +0330

    add __init__

--- restaurant/main.py
+++ restaurant/main.py
@@ -1,15 +1,10 @@
 from sqlalchemy import create_engine
 
-from restaurant.models import superadmin, admin, customer, category, item, order, order_item, base
+from restaurant.models import SuperAdmin, Admin
 
 engine = create_engine('postgresql+psycopg2://kian:bmw1386z4@localhost:5432/restaurant', echo=True)
 
-print(superadmin,
-      admin,
-      customer,
-      category,
-      item,
-      order,
-      order_item,
-      base)
-
+print(
+      SuperAdmin,
+      Admin
+)
---
+++ restaurant/models/__init__.py
@@ -0,0 +1,9 @@
+from restaurant.models.admin import Admin
+from restaurant.models.base import Base
+from restaurant.models.category import Category
+from restaurant.models.customer import Customer
+from restaurant.models.item import Item
+from restaurant.models.order import Order
+from restaurant.models.order_item import OrderItem
+from restaurant.models.superadmin import SuperAdmin
+
