f8e3655e2a4305e2bb60787f92913b94bd459d32
kian <wk111111111kw@gmail.com>
2023-11-27 19:34:10 +0330

    Update

--- restaurant/main.py
+++ restaurant/main.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 
 from restaurant.moduls.base import Base
-from restaurant.moduls import SuperAdmin, Admin, Customer, Category, Item, Order, OrderItem
+from restaurant.moduls import SuperAdmin, Admin, Customer, Category, Item, Order, OrderItem, Payment
 
 engine = create_engine('postgresql+psycopg2://kian:bmw1386z4@localhost:5432/restaurant', echo=True)
 Base.metadata.create_all(engine)
